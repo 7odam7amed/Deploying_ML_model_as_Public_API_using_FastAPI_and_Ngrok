f1987631bc0d4b03c16dfa0e6f544d1bbab10e80
7odam7amed <7odam7amed@gmail.com>
2026-04-23 22:00:33 +0200

    Implementation

---
+++ api_implementation.py
@@ -0,0 +1,22 @@
+import json
+import requests
+
+url = 'http://127.0.0.1:8000/diabetes_prediction'
+
+
+
+input_data_for_model = {
+    'Pragnencies' : 6, 
+    'Glucose' : 148,
+    'BloodPressure' : 72, 
+    'SkinThickness' : 35,
+    'Insulin' : 0,
+    'BMI' : 33.6,
+    'DiabetesPedigreeFunction' : 0.627,
+    'Age' : 50
+}
+
+
+response = requests.post(url, json=input_data_for_model)
+
+print(response.text)
